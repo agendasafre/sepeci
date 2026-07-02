ccfa09132030ae2aa9426759fd4eaab0f5bb3afd
andpetronio <andpetronio@gmail.com>
2026-07-02 09:58:09 -0300

    real expiration date

--- form-config.js
+++ form-config.js
@@ -1,8 +1,6 @@
 (function exposeFormConfig(globalScope) {
   const formConfig = {
-    // Desde las 00:00 de esta fecha el formulario deja de mostrarse.
-    // Cambiá solo este valor cuando definas la fecha final.
-    expiresAt: "2026-06-30T00:00:00-03:00",
+    expiresAt: "2026-07-30T00:00:00-03:00",
     expiredMessage:
       "El formulario de inscripción ya no se encuentra disponible.",
   };
