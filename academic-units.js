f388169e456aa5e52a267450b6d66c33ca7e26bc
andpetronio <andpetronio@gmail.com>
2026-07-02 09:47:42 -0300

    unidades académicas

--- academic-units.js
+++ academic-units.js
@@ -1,10 +1,14 @@
 (function exposeAcademicUnits(globalScope) {
   const academicUnits = [
-    { id: "rectorado", name: "Rectorado" },
-    { id: "facultad-ciencias-exactas", name: "Facultad de Ciencias Exactas" },
-    { id: "facultad-ciencias-humanas", name: "Facultad de Ciencias Humanas" },
+    { id: "facultad-ingenieria", name: "Facultad de Ingeniería"}
+    { id: "facultad-ciencias-exactas", name: "Facultad de Ciencias Exactas, Físicas y Naturales" },
+    { id: "facultad-filosofia", name: "Facultad de Filosifía, Humanidades y Artes" },
     { id: "facultad-ciencias-sociales", name: "Facultad de Ciencias Sociales" },
-    { id: "facultad-ingenieria", name: "Facultad de Ingeniería" },
+    { id: "facultad-arquitectura", name: "Facultad de Arquitectura, Urbanismo y Diseño" },
+    { id: "escuela-salud", name: "Escuela Universitaria de Ciencias de la Salud" },
+    { id: "ipu-comercio", name: 'IPU - Escuela de Comercio "Libertador Gral. San Martín"' },
+    { id: "ipu-industrial", name: 'IPU - Escuela Industrial "Domingo F. Sarmiento"' },
+    { id: "ipu-central", name: 'IPU - Colegio Central Universitario "Mariano Moreno"' },
     { id: "otra-unidad-academica", name: "Otra unidad académica" },
   ];
 
